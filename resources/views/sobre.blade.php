b7c0c0b57e18d80cce6ac00b6a12952d1bc9c03e
Fabiana Mendes <dev.fabianamendes@gmail.com>
2026-04-01 00:19:37 -0300

    adicionando nome do grupo na pagina

--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -6,5 +6,15 @@
 <body>
     <h1>Página Sobre</h1>
     <p>{{ $mensagem }}</p>
+    <br>
+    <p><strong>Grupo</strong><br> </p>
+    <ul>
+        <li>Fabiana Mendes</li>
+        <li>Tiago Vieira</li>
+        <li>Tiago Silva</li>
+           
+    </ul>       
+
+
 </body>
 </html>
